c0ced46db806ede79d6e1efa2f1430e6f28485a0
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-08-19 12:23:01 +0545

    Added search design
    search icon and design is added.

--- oe_search/lib/responsive/web_screen_layout.dart
+++ oe_search/lib/responsive/web_screen_layout.dart
@@ -1,62 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:oe_search/responsive/color.dart';
+import 'package:oe_search/widgets/search.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: backgroundColor,
-        elevation: 0,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Gmail",
-              style: TextStyle(
-                color: primaryColor,
-                fontWeight: FontWeight.w400,
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Gmail",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "Images",
-              style: TextStyle(
-                color: primaryColor,
-                fontWeight: FontWeight.w400,
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Images",
+                style: TextStyle(
+                  color: primaryColor,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/images/more-apps.svg",
-              color: primaryColor,
+            const SizedBox(
+              width: 10,
             ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          MaterialButton(
-            onPressed: () {},
-            child: const Text(
-              "Sign in",
-              style: TextStyle(color: Colors.white),
+            IconButton(
+                icon: SvgPicture.asset('images/more-apps.svg',
+                    color: primaryColor),
+                onPressed: () {}),
+            const SizedBox(
+              width: 10,
             ),
-          ),
-        ],
-      ),
-      body: Text("Hello from Web"),
-    );
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10).copyWith(
+                right: 10,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                color: const Color(0xff1A73EB),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 5, right: 5),
+          child: Column(children: [
+            SizedBox(
+              height: size.height * 0.25,
+            ),
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Search(),
+                  ],
+                )
+              ],
+            ))
+          ]),
+        ));
   }
 }
---
+++ oe_search/lib/widgets/search.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:oe_search/responsive/color.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Center(
+          child: Image.asset(
+            'images/google-logo.png',
+            height: size.height * 0.12,
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: size.width * 0.4,
+          child: TextFormField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: searchBorder,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/search-icon.svg',
+                  color: searchBorder,
+                  width: 1,
+                  height: 1,
+                ),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(
+                  'images/mic-icon.svg',
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
